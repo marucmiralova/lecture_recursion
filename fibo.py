242f67e62ddd4ddb9c5fa710f4434bbf68fba32d
marucmiralova <marucmiralova@seznam.cz>
2025-04-28 10:02:18 +0200

    hotovy fibo

--- fibo.py
+++ fibo.py
@@ -1,6 +1,17 @@
 
+def recursive_nth_fibo(idx):
+    posloupnost = [0,1]
+    if idx == 0:
+        return posloupnost[idx]
+    elif idx  == 1:
+        return posloupnost[idx]
+    else:
+        return recursive_nth_fibo(idx-1) + recursive_nth_fibo(idx-2) #vrati to finalni cislo
+
 def main():
-    pass
+    n = int(5)
+    seq = [recursive_nth_fibo(num) for num in range(n+1)]
+    print(seq)
 
 
 if __name__ == "__main__":
